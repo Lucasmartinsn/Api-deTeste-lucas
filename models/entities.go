73b5428862db23861af69564a1a09c6e8d9536c2
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-10-13 11:13:42 -0300

    up

--- models/entities.go
+++ models/entities.go
@@ -2,9 +2,9 @@ package models
 
 /// Tabela com os typos e os campos da tabela
 type Cadastro struct {
-	ID         		int  `json:"id"`
-	Foto 			string  `json:"foto"`
-	Nome_usuario  	string  `json:"nome_usuario"`
-	Email 			string	`json:"Email"`
-	Senha  			int 	`json:"Senha"`
+	ID           int    `json:"id"`
+	Foto         string `json:"foto"`
+	Nome_usuario string `json:"nome_usuario"`
+	Email        string `json:"Email"`
+	Senha        int    `json:"Senha"`
 }
--- models/insert.go
+++ models/insert.go
@@ -1,21 +1,20 @@
 package models
 
 import (
-
 	"github.com/Lucasmartinsn/ApiProjeto-da-tropa/db"
 )
 
-func Insert(cadastro Cadastro) ( id int, err error ){
+func Insert(cadastro Cadastro) (id int, err error) {
 	conn, err := db.OpenConnection()
-	if err != nil{
+	if err != nil {
 		return
 	}
 
 	defer conn.Close()
 
-	sql := `INSERT INTO cadastro (id, foto, nome_usuario, email, senha) VALUES ($1,$2,$3,$4,$5) RETURNING id`
-	
+	sql := `INSERT INTO cadastro (foto, nome_usuario, email, senha) VALUES ($1,$2,$3,$4) RETURNING id`
+
 	err = conn.QueryRow(sql, cadastro.Foto, cadastro.Nome_usuario, cadastro.Email, cadastro.Senha).Scan(&id)
-	
+
 	return
 }
